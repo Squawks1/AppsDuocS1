1f09c6712253a7eb35e657ad5b57abc250747f55
Squawks1 <to.toledom@duocuc.cl>
2025-11-02 16:21:43 -0300

    Update login.page.ts
    se agregan las validaciones para la página de login, así como mensajes de alerta/error

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
   standalone: false,
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
+
+  constructor(private alertController: AlertController,
+              private router: Router
+  ) { }
+
+  //Método para mostrar una alerta de error
+  async mostrarAlerta(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: mensaje,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  //Función para validar el formato de correo electrónico
+  validarEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email)
+  }
+
+  //Función para validar los componentes para iniciar sesión
+  login() {
+
+    //Validar que el campo del correo electrónico no esté vacío
+    if (!this.email) {
+      this.mostrarAlerta('El campo del correo no puede estar vacío');
+      return;
+    }
+
+    //Validar que el formato del correo sea el correcto
+    if (!this.validarEmail(this.email)) {
+      this.mostrarAlerta('El formato del correo es inválido');
+      return;
+    }
+
+    //Verificar que la contraseña no esté vacía
+    if (!this.password) {
+      this.mostrarAlerta('El campo de la contraseña no puede estar vacío');
+      return;
+    }
+
+    //Verificar que la contraseña tenga un mínimo de longitud
+    if (this.password.length < 6) {
+      this.mostrarAlerta('La contraseña debe tener un mínimo de 6 caracteres');
+      return;
+    }
+
+    //Si pasa todas las validaciones, que redireccione a la página home
+    this.router.navigate(['/home'], { state: { user: this.email } });
+
+  }
+
+  /* No Todavía
 
   ngOnInit() {
   }
+  */
 
 }
