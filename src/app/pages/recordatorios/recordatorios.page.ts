255d075b649229173ed6459a5181d0d86a1b8464
Squawks1 <to.toledom@duocuc.cl>
2025-11-10 22:52:54 -0300

    Update recordatorios.page.ts
    Se agregan las opciones de registrar y eliminar los recordatorios. Se evalúa también el uso de notificaciones push a futuro

--- src/app/pages/recordatorios/recordatorios.page.ts
+++ src/app/pages/recordatorios/recordatorios.page.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recordatorios',
   templateUrl: './recordatorios.page.html',
   styleUrls: ['./recordatorios.page.scss'],
+  standalone: false,
 })
-export class RecordatoriosPage implements OnInit {
+export class RecordatoriosPage {
 
-  constructor() { }
+  recordatorios: { medicamento: string; hora: string; nota?: string }[] = [];
+  nuevoMedicamento = '';
+  nuevaHora = '';
+  nuevaNota = '';
 
-  ngOnInit() {
+  constructor(private alertController: AlertController) {}
+
+  // Agregar un nuevo recordatorio
+  async agregarRecordatorio() {
+    if (!this.nuevoMedicamento || !this.nuevaHora) {
+      const alert = await this.alertController.create({
+        header: 'Campos obligatorios',
+        message: 'Debe ingresar un medicamento y una hora.',
+        buttons: ['Aceptar']
+      });
+      await alert.present();
+      return;
+    }
+
+    this.recordatorios.push({
+      medicamento: this.nuevoMedicamento,
+      hora: this.nuevaHora,
+      nota: this.nuevaNota
+    });
+
+    this.nuevoMedicamento = '';
+    this.nuevaHora = '';
+    this.nuevaNota = '';
   }
 
+  // Eliminar un recordatorio
+  eliminarRecordatorio(index: number) {
+    this.recordatorios.splice(index, 1);
+  }
+
+  /* Se podría immplementar notificaciones para recordar al 
+  usuario cuándo debe tomar el medicamento */
 }
